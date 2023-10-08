ff4bf20e9f1754d80bc872e0da4e7fc9f9f95624
Tim Petri <tim.a.petri@gmail.com>
2023-10-08 16:25:51 -0400

    Title and fix animation

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ const SOCIALS = [
     icon: GithubIcon,
   },
   {
-    name: "Linked In",
+    name: "LinkedIn",
     url: "https://www.linkedin.com/in/timpetri/",
     icon: LinkedinIcon,
   },
@@ -24,7 +24,7 @@ export default function Home() {
     <main className=" prose min-h-screen m-auto pt-16 sm:px-2 px-6">
       <h1 className="">Tim Petri</h1>
       <div className="slide-in-content">
-        <p className="">Hey, I&apos;m Tim Petri,</p>
+        <p className="">Hey, I&apos;m Tim Petri.</p>
         <p>
           I&apos;m a founding engineer at{" "}
           <a href="https://www.archlending.com/" target="_blank">
@@ -34,15 +34,15 @@ export default function Home() {
         </p>
         <p>
           Before that, I spent a number of years at Google working on consumer
-          products, including Maps, Search and Lens.
+          products, including Maps, Search, and Lens.
         </p>
         <p>
-          My interests span crypto, AI, philosophy, consciousness and history.
+          My interests span crypto, AI, philosophy, consciousness, and history.
           If you&apos;re interested in any of these topics, I&apos;d love to
           chat.
         </p>
         <p>I grew up in Sweden and speak Swedish, English, and German.</p>
-        <p>Find me on </p>
+        <p>Find me on: </p>
         <div className="flex space-x-1">
           {SOCIALS.map((social) => (
             <Link
@@ -57,6 +57,7 @@ export default function Home() {
           ))}
         </div>
       </div>
+      <p className="hidden animate-slide-in"></p>
     </main>
   );
 }
